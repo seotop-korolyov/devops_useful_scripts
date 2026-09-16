f3954109b2b3f3a16bc695b15ed8770a7f6d4512
Artem Korolov <seotop.korolyov@gmail.com>
2026-09-16 19:03:04 +0200

    Host name

--- server_health.py
+++ server_health.py
@@ -1,1 +1,12 @@
-#!/use/bin/python3
+#!/usr/bin/python3
+import subprocess
+
+result = subprocess.run(
+    "hostname",
+    capture_output=True,
+    text=True
+)
+
+hostname = result.stdout.strip()
+print("=== SERVER HEALTH ===")
+print(f"Hostname: {hostname}")
