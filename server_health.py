2980f6bd5482fa61e4aaed03f8b146b9e0733b2c
Artem Korolov <seotop.korolyov@gmail.com>
2026-09-17 19:21:08 +0200

    Add memory usage

--- server_health.py
+++ server_health.py
@@ -33,7 +33,7 @@ def memory_usage():
         capture_output=True,
         text=True
     )
-    return memory
+    return memory.stdout()
 
 disk_percent, disk_message = disk_usage()
 print(memory_usage())
