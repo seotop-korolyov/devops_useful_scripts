17a011b81bc6bceb244815f86570b56ff992a581
Artem Korolov <seotop.korolyov@gmail.com>
2026-09-19 19:20:08 +0200

    Refactor Count CPU

--- server_health.py
+++ server_health.py
@@ -85,7 +85,7 @@ def health_status(percent):
     return message
 
 #Format metric
-def format_metric(value, status="", unit=""):
+def format_metric(value, status=None, unit=""):
     if value is None:
         return "UNKNOWN!"
 
