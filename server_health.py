b4fb649dfd241d5d5b89bf7d9fb281512126043d
Artem Korolov <seotop.korolyov@gmail.com>
2026-09-19 19:42:47 +0200

    Refactor Count CPU

--- server_health.py
+++ server_health.py
@@ -51,7 +51,7 @@ def load_average():
 
 #Count CPU
 def count_cpu():
-    count_cpu = run_command(["nprocc"])
+    count_cpu = run_command(["nproc"])
     if count_cpu is None:
         return None
     return int(count_cpu.strip())
