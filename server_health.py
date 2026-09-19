79e571b05a6ad837c2ca8dcdfdd0d9c2ec2d9a1c
Artem Korolov <seotop.korolyov@gmail.com>
2026-09-19 18:49:15 +0200

    Refactoring Load average

--- server_health.py
+++ server_health.py
@@ -43,7 +43,7 @@ def memory_usage():
 
 #Load average
 def load_average():
-    load = run_command(["catt", "/proc/loadavg"])
+    load = run_command(["cat", "/proc/loadavg"])
     if load is None:
         return None, None, None
     load_1min = load.strip().split()
