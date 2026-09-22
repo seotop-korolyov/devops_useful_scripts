b87eb17f5e5315044e99a5e9efb350a9e94df050
Artem Korolov <seotop.korolyov@gmail.com>
2026-09-22 18:55:31 +0200

    Add Check Docker containers

--- server_health.py
+++ server_health.py
@@ -93,7 +93,8 @@ def load_status(load, cpus):
 
 #Docker Containers
 def docker_containers():
-    containers = run_command(["docker", "ps", "--format", "{{.Name}}"])
+    #containers = run_command(["docker", "ps", "--format", "{{.Name}}"])
+    containers = run_command(["docker", "ps"])
     if containers is None:
         return None
     return containers.strip().split()
