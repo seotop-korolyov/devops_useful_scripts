088f10a57eb92d787b5f07b906bbf7798f364b37
Artem Korolov <seotop.korolyov@gmail.com>
2026-09-23 19:18:59 +0200

    Add Check containers health status

--- server_health.py
+++ server_health.py
@@ -180,6 +180,7 @@ elif containers:
             health_message = "NO HEALTHCHECK"
 
         #2nd way to find out the Health status
+        print(container[0])
         container_health_2 = container_health(container[0])
         if container_health_2 == "healthy":
             health_message_2 = "HEALTHY"
