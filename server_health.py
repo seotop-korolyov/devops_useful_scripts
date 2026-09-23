97e519673ce41824df383c6109eaadeaaf3eae02
Artem Korolov <seotop.korolyov@gmail.com>
2026-09-23 17:04:32 +0200

    Add Check Docker containers

--- server_health.py
+++ server_health.py
@@ -159,7 +159,6 @@ if containers is None:
 elif containers:
     for container in containers:
         container = container.split()
-        print(container[-1].strip("()"))
         if container[-1].strip("()") == "healthy":
             container_health = "HEALTHY"
         else:
