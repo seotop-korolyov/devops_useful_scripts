ba1a0fe5fa0e39e567c5aef659ff7ab13d30e1ad
Artem Korolov <seotop.korolyov@gmail.com>
2026-09-24 18:23:10 +0200

    Implement Exit Codes

--- server_health.py
+++ server_health.py
@@ -182,6 +182,13 @@ else:
 print("\n\n")
 
 #Exit Code
-if disk_health  == "CRITICAL!!!" or memory_health == "CRITICAL!!!" or load_status_1min == "CRITICAL!!!" or load_status_5min == "CRITICAL!!!" or load_status_15min == "CRITICAL!!!":
+statuses = [
+    disk_health,
+    memory_health,
+    load_status_1min,
+    load_status_5min,
+    load_status_15min
+]
+if any(statuses):
     exit_code = 1
 sys.exit(exit_code)
