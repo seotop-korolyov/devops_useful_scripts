8ac8e88fe77c2c79d9b64e1f7f19ee098702fbf9
Artem Korolov <seotop.korolyov@gmail.com>
2026-09-24 18:47:37 +0200

    Overall status

--- server_health.py
+++ server_health.py
@@ -193,7 +193,9 @@ statuses = [
     load_status_15min
 ]
 
-if any(status == "CRITICAL!!!" for status in statuses) or docker == "NOT RUNNING" or any(container_status == "UNHEALTHY" for container_status in container_statuses):
+if any(status == "CRITICAL!!!" for status in statuses) \
+    or docker == "NOT RUNNING" \
+    or any(container_status == "UNHEALTHY" for container_status in container_statuses):
     exit_code = 1
     print("=== SUMMARY ===")
     print("Overall status: [ CRITICAL!!! ]")
