b845d90a2420d06e5334007052551fb9ad00bddc
Artem Korolov <seotop.korolyov@gmail.com>
2026-09-25 18:26:21 +0200

    Add logs

--- server_health.py
+++ server_health.py
@@ -208,7 +208,7 @@ if any(status == "CRITICAL!!!" for status in statuses) \
     exit_code = 1
     print("=== SUMMARY ===")
     print("Overall status: [ CRITICAL!!! ]")
-    write_log({"Disk": [disk_metric, "%", disk_health],
+    write_log({"Disk": disk_metric,
                "Memory Usage": [memory_use, "%", memory_health],
                "Load average": [load_1min, load_status_1min],
                "Docker": docker
