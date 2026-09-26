68fd53a70ffb3a3b213bd945746c5cc49935797a
Artem Korolov <seotop.korolyov@gmail.com>
2026-09-26 18:20:22 +0200

    Add logs

--- server_health.py
+++ server_health.py
@@ -195,8 +195,6 @@ if containers is None:
 elif containers:
     for container in containers:
         container_status = container_health(container)
-        if container == "parser-php-1" or container == "repositry-registry-1":
-            container_status = "UNHEALTHY"
         if container_status == "UNHEALTHY":
             container_statuses_log[container] = container_status
         print(f"{container} \t [ {container_status} ]")
